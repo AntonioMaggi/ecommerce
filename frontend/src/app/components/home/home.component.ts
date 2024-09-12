2d52c94e04ccf674fc2e408547cdc4083307a7ed
Antonio Maggi <maggia1013@gmail.com>
2024-09-12 15:17:09 +0200

    added some basic css

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService, Product } from '../../services/product.service';
 import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+import { NgFor } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
-  providers: [ProductService]
+  styleUrls: ['./home.component.css'],
+  imports: [NgFor, CommonModule, HttpClientModule],
+  providers: [ProductService ],
 })
 export class HomeComponent implements OnInit {
   products: Product[] = [];
@@ -17,8 +19,18 @@ export class HomeComponent implements OnInit {
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe((data) => {
-      this.products = data;
+    this.loadProducts();
+  }
+
+  // Fetch products from the backend
+  loadProducts(): void {
+    this.productService.getProducts().subscribe({
+      next: (response) => {
+        this.products = response;
+      },
+      error: (error) => {
+        console.error('Error fetching products:', error);
+      },
     });
   }
 }
--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,15 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-export interface Product {
-  id?: number;
-  name: string;
-  description?: string;
-  price: number;
-  image_url?: string;
-  category_id?: number;
-}
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root',
